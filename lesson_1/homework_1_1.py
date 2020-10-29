6f7e9a85cd62f72329fabf9f811353a82cb55be1
amber.lowe <amberlowe1001>
2020-10-28 23:34:23 -0700

    final commit

--- lesson_1/homework_1_1.py
+++ lesson_1/homework_1_1.py
@@ -2,52 +2,63 @@
 
 a = 10
 b = 15
-result_sum = None
+result_sum = 10 + 15
+print(result_sum)
 
 
 # Find the difference between c and d. Save the expression to the result_diff variable
 
 c = 10
 d = 15
-result_diff = None
+result_diff = 10-15
+print(result_diff)
+
 
 
 # Multiply e by f. Save the expression to the result_multiplication variable
 
 e = 10
 f = 15
-result_multiplication = None
+result_multiplication = 10 * 15
+print(result_multiplication)
 
 
 # Divide g by h. Save the expression to the result_division variable
 
 g = 10
 h = 15
-result_division = None
+result_division = 10 / 15
+print(result_division)
+
 
 
 # Calculate the floored division result of i and j. Save the expression to the result_division_floored variable
 
 i = 10
 j = 15
-result_division_floored = None
+result_division_floored = 10 // 15
+print(result_division_floored)
+
 
 
 # Calculate the remainder of k divided by l. Save expression to the result_division_remainder variable
 
 k = 10
 l = 15
-result_division_remainder = None
+result_division_remainder = 10 % 15
+print(result_division_remainder)
 
 
 # Rise m to the power n. Save the expression to the result_power variable
 
 m = 5
 n = 2
-result_power = None
+result_power = pow(5,2)
+print(result_power)
 
 
 # Convert an o variable to negative. Save the expression to the result_negative variable
 
 o = 100
-result_negative = None
+result_negative = abs(-100)
+print(result_negative)
